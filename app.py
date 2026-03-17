b7e9c20b255c80788d63dbef03c2b03d31f15194
yoriichi123451 <ilaakusen5@gmail.com>
2026-03-17 18:58:02 +0200

    Changed README.md

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send, emit
+
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "Socket5196"
+socketio = SocketIO(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@socketio.on('message')
+def handle_message(data):
+    print('received message: ' + data)
+    send(data, broadcast=True)
+
+if __name__ == "__main__":
+    socketio.run(app, allow_unsafe_werkzeug=True)
